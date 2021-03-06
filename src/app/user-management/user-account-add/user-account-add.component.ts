df217471a1c7bbcc15e58710e556419d4f1099cc
Eanda <eandalined@gmail.com>
2021-03-06 16:54:50 +0700

    create component user-namagement

---
+++ src/app/user-management/user-account-add/user-account-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-add',
+  templateUrl: './user-account-add.component.html',
+  styleUrls: ['./user-account-add.component.css']
+})
+export class UserAccountAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user-management/user-management.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user/user.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { UserAccountAddComponent } from './user-account-add/user-account-add.component';
+import { UserAccountEditComponent } from './user-account-edit/user-account-edit.component';
+import { UserRoleComponent } from './user-role/user-role.component';
+import { UserRoleAddComponent } from './user-role-add/user-role-add.component';
+import { UserRoleEditComponent } from './user-role-edit/user-role-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserComponent,
+    UserAddComponent,
+    UserEditComponent,
+    UserAccountComponent,
+    UserAccountAddComponent,
+    UserAccountEditComponent,
+    UserRoleComponent,
+    UserRoleAddComponent,
+    UserRoleEditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserManagementModule { }
